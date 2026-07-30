7c16a905c881b2648ab09e380d6d0903c74baf38
FattiMei <teomei68@gmail.com>
2026-07-30 08:58:47 +0200

    compiler: refactor external source compilation into single file

--- peppo/ext_source.py
+++ peppo/ext_source.py
@@ -1,8 +1,39 @@
 from enum import Enum
+from subprocess import run, CalledProcessError
 from dataclasses import dataclass
 
-import peppo.clang
+
 import llvmlite.binding as llvm
+
+
+def _clang_compile_command(language: str,
+                           defines: list[str] = None,
+                           include_directories: list[str] = None) -> list[str]:
+    """
+    Returns the clang command for an unoptimized build
+    that outputs llvm ir
+
+    An unoptimized build will automatically insert function
+    attributes like `noopt` and `noinline` which interfere
+    with later optimization passes
+    """
+    cmd = [
+            'clang',
+            '-S', '-emit-llvm',
+            '-O1', '-mllvm', '-disable-llvm-optzns', '-disable-llvm-passes',
+            '-x', language, '-',
+            '-o', '-'
+            ]
+
+    if include_directories is not None:
+        for include_dir in include_directories:
+            cmd.extend(('-I', include_dir))
+
+    if defines is not None:
+        for define in defines:
+            cmd.extend(('-D', define))
+
+    return cmd
 
 
 class Language(Enum):
@@ -18,9 +49,21 @@ class ExtSource:
     defines: list[str] = None
     include_directories: list[str] = None
 
-    def compile(self) -> llvm.ModuleRef:
-        return peppo.clang.compile(
-                self.src,
+    def compile_to_llvm_ir(self) -> llvm.ModuleRef:
+        clang_compile_cmd = _clang_compile_command(
                 self.language.value,
                 self.defines,
                 self.include_directories)
+
+        res = run(
+                clang_compile_cmd,
+                input=self.src.encode(),
+                capture_output=True,
+                check=True)
+
+        llvm_ir = res.stdout.decode()
+        module = llvm.parse_assembly(llvm_ir)
+        module.verify()
+
+        return module
+
