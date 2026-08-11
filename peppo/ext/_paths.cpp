9fbc5f09955d38eee7f6495a261766630cdaf1f4
FattiMei <teomei68@gmail.com>
2026-08-11 17:10:02 +0200

    package: add a CMake configuration for coupling python and external tools

---
+++ peppo/ext/_paths.cpp
@@ -0,0 +1,48 @@
+#include <pybind11/pybind11.h>
+
+
+// CMake should provide this translation unit with the paths of the LLVM/MLIR tools
+// As a form of defensive programming, the missing entries are set to empty strings
+//
+// I hope this is a simple enough mechanism for registering the external tools
+// It shouldn't be modified very much
+#ifndef CLANG_PATH
+#define CLANG_PATH ""
+#endif
+
+#ifndef OPT_PATH
+#define OPT_PATH ""
+#endif
+
+#ifndef FLANG_PATH
+#define FLANG_PATH ""
+#endif
+
+#ifndef MLIR_OPT_PATH
+#define MLIR_OPT_PATH ""
+#endif
+
+#ifndef CLANGIR_PATH
+#define CLANGIR_PATH ""
+#endif
+
+
+PYBIND11_MODULE(_paths, m) {
+	m.doc() = "Bridge module to import the LLVM tool paths";
+
+	// all these functions return an empty string when the program is not found
+	m.def("get_clang_path",
+	      []() { return std::string(CLANG_PATH); });
+
+	m.def("get_opt_path",
+	      []() { return std::string(OPT_PATH); });
+
+	m.def("get_flang_path",
+	      []() { return std::string(FLANG_PATH); });
+
+	m.def("get_mlir_opt_path",
+	      []() { return std::string(MLIR_OPT_PATH); });
+
+	m.def("get_clangir_path",
+	      []() { return std::string(CLANGIR_PATH); });
+}
