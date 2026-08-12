c92936a014ed883c3e87f98d34949cbdb84c9a01
FattiMei <teomei68@gmail.com>
2026-08-12 16:58:16 +0200

    package: add utils function to print the installation environment

---
+++ peppo/utils.py
@@ -0,0 +1,26 @@
+from peppo._env import (
+    get_clang_path,
+    get_opt_path,
+    get_flang_path,
+    get_mlir_opt_path,
+    get_clangir_path,
+    get_llvm_include_dirs,
+    get_llvm_definitions,
+)
+
+
+ENV = {
+    'CLANG_PATH'       : get_clang_path,
+    'OPT_PATH'         : get_opt_path,
+    'FLANG_PATH'       : get_flang_path,
+    'MLIR_OPT_PATH'    : get_mlir_opt_path,
+    'CLANGIR_PATH'     : get_clangir_path,
+    'LLVM_INCLUDE_DIRS': get_llvm_include_dirs,
+    'LLVM_DEFINITIONS' : get_llvm_definitions,
+}
+
+
+def dump_environment():
+    for name, path_fun in ENV.items():
+        print(name, ": ", path_fun())
+
