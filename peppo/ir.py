6fab3079a74e60ccd02e7d1e2f1c47be1d28dd89
Mei Matteo <teomei68@gmail.com>
2026-08-29 17:30:41 +0200

    dsl: solved the problem of recursive functions with decorators

--- peppo/ir.py
+++ peppo/ir.py
@@ -1,39 +1,67 @@
+import inspect
 from enum import Enum
+from collections.abc import Callable
 from dataclasses import dataclass
 
 
-class Type(Enum):
-    SCALAR = 0
-
-
-class Value:
+class DType:
     pass
 
 
-class Constant(Value):
-    def __init__(self, value, dtype: Type = None):
-        self.value = value
+class Value:
+    def __init__(self, name: str = None, dtype: DType = None):
+        self.name = name
         self.dtype = dtype
 
 
-@dataclass
-class Mul(Value):
-    left:  Value
-    right: Value
-    dtype: Type = None
-    name:  str = None
+class Constant(Value):
+    def __init__(self, value, name: str = None):
+        super().__init__(name=name)
+        self.value = value
 
 
-@dataclass
-class Add(Value):
-    left:  Value
-    right: Value
-    dtype: Type = None
-    name:  str = None
+class BinExpr(Value):
+    def __init__(self, left: Value, right: Value, name: str = None):
+        super().__init__(name=name)
+        self.left = left
+        self.right = right
 
 
-@dataclass
-class Function(Value):
-    signature: tuple[Type]
-    body: Value = None
+# this is not part of the ir, but it's necessary to define procedures as decorated python functions
+class Function:
+    """
+    This class makes possible to write DSL functions as decorated python functions.
+    With this class one should be able to produce a LLVM module
+    """
+    def __init__(self, signature: tuple[DType], body: Callable[..., Value], name: str = None):
+        self.signature = signature
+        self.body = body
+        self.name = name
+
+    def __call__(self, *args):
+        """
+        This method is used when a DSL function is called within a DSL function
+        We just return an IR node that doesn't expand the function body
+
+        This call should validate the arguments against the signature
+        """
+        return FunctionCall(self, *args)
+
+
+class FunctionCall(Value):
+    # this should add the `name` argument
+    def __init__(self, func: Function, *args):
+        self.func = func
+
+
+def proc(f):
+    """
+    This decorator makes the embedded DSL possible.
+    With a clever trick we can capture the body of the function and decorate it at the same time.
+    The result is a callable Function object that can be transformed as the user requires
+    """
+    return Function(
+        signature=inspect.signature(f),
+        body=f,
+        name=f.__name__)
 
